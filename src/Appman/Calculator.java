8b0046e861ab987f31edffacf6ffc7e7a97decc1
sirawit1997 <Sirawi.u@ku.th>
2020-07-07 22:31:09 +0700

    นาย สิรวิชญ์ อุปการ ส่ง Test string2int Challenge

---
+++ src/Appman/Calculator.java
@@ -0,0 +1,70 @@
+package Appman;
+
+import javafx.event.EventDispatchChain;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    String textinput ;
+    ArrayList<Integer> number ;
+    public Calculator(String textinput) {
+        this.textinput = textinput;
+    }
+
+    public void splitInteger()
+    {
+        number = new ArrayList<>() ;
+        for (int i=0; i <textinput.length(); i++)
+        {
+            if (Character.isDigit(textinput.charAt(i)))
+            {
+                if (textinput.charAt(i) == '0')
+                {
+                    number.add(0) ;
+                }
+                else if (textinput.charAt(i) == '1')
+                {
+                    number.add(1) ;
+                }
+                else if (textinput.charAt(i) == '2')
+                {
+                    number.add(2) ;
+                }
+                else if (textinput.charAt(i) == '3')
+                {
+                    number.add(3) ;
+                }
+                else if (textinput.charAt(i) == '4')
+                {
+                    number.add(4) ;
+                }
+                else if (textinput.charAt(i) == '5')
+                {
+                    number.add(5) ;
+                }
+                else if (textinput.charAt(i) == '6')
+                {
+                    number.add(6) ;
+                }
+                else if (textinput.charAt(i) == '7')
+                {
+                    number.add(7) ;
+                }
+                else if (textinput.charAt(i) == '8')
+                {
+                    number.add(8) ;
+                }
+                else if (textinput.charAt(i) == '9')
+                {
+                    number.add(9) ;
+                }
+            }
+        }
+
+        System.out.print("The output should be ");
+        for (int i = 0; i < number.size(); i++) {
+            System.out.print(number.get(i));
+        }
+        System.out.print(" in integer.");
+    }
+}
