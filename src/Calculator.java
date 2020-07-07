cbd47308352dc526eb043873aaaf54d39f024d70
sirawit1997 <Sirawi.u@ku.th>
2020-07-07 23:54:49 +0700

    นาย สิรวิชญ์ อุปการ ส่ง Test string2int Challenge

--- src/Calculator.java
+++ src/Calculator.java
@@ -16,46 +16,16 @@ public class Calculator {
         {
             if (Character.isDigit(textinput.charAt(i)))
             {
-                if (textinput.charAt(i) == '0')
-                {
-                    number.add(0) ;
-                }
-                else if (textinput.charAt(i) == '1')
-                {
-                    number.add(1) ;
-                }
-                else if (textinput.charAt(i) == '2')
-                {
-                    number.add(2) ;
-                }
-                else if (textinput.charAt(i) == '3')
-                {
-                    number.add(3) ;
-                }
-                else if (textinput.charAt(i) == '4')
-                {
-                    number.add(4) ;
-                }
-                else if (textinput.charAt(i) == '5')
-                {
-                    number.add(5) ;
-                }
-                else if (textinput.charAt(i) == '6')
-                {
-                    number.add(6) ;
-                }
-                else if (textinput.charAt(i) == '7')
-                {
-                    number.add(7) ;
-                }
-                else if (textinput.charAt(i) == '8')
-                {
-                    number.add(8) ;
-                }
-                else if (textinput.charAt(i) == '9')
-                {
-                    number.add(9) ;
-                }
+                if (textinput.charAt(i) == '0')  number.add(0) ;
+                else if (textinput.charAt(i) == '1') number.add(1) ;
+                else if (textinput.charAt(i) == '2') number.add(2) ;
+                else if (textinput.charAt(i) == '3') number.add(3) ;
+                else if (textinput.charAt(i) == '4') number.add(4) ;
+                else if (textinput.charAt(i) == '5') number.add(5) ;
+                else if (textinput.charAt(i) == '6') number.add(6) ;
+                else if (textinput.charAt(i) == '7') number.add(7) ;
+                else if (textinput.charAt(i) == '8') number.add(8) ;
+                else if (textinput.charAt(i) == '9') number.add(9) ;
             }
         }
 
--- src/main.java
+++ src/main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 ////////////////  Sirawit Uppakan K.moei  /////////////////
 public class main {
     public static void main(String[] args) {
-        System.out.println("Given input is :");
+        System.out.print("Given input is : ");
         Scanner scan = new Scanner(System.in);
-        String textinput = scan.next();
+        String textinput = scan.nextLine();
         Calculator calculator = new Calculator(textinput);
         calculator.splitInteger();
     }
